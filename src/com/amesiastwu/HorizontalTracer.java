b6ef7c4613deed5b15b38e5a6c3934f5fa32f68e
alejandrom <amesias@thoughtworks.com>
2018-02-27 11:49:03 -0500

    Print an asterisks line, ok

---
+++ src/com/amesiastwu/HorizontalTracer.java
@@ -0,0 +1,12 @@
+package com.amesiastwu;
+
+public class HorizontalTracer {
+    public void draw(int length){
+        final String CHARACTER = "*";
+
+        for(int i = 0 ; i < length; i++){
+            System.out.print(CHARACTER);
+        }
+
+    }
+}
--- src/com/amesiastwu/Main.java
+++ src/com/amesiastwu/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Print one Asterisk:");
-	    System.out.print("*");
+	    System.out.println("*");
+
+	    HorizontalTracer line = new HorizontalTracer();
+	    int length = 10;
+        System.out.println("Print a line:");
+	    line.draw(length);
     }
 }
