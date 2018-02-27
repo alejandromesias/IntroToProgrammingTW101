1750bf379b9af5df2ebb51184c8f72b47fc4d035
alejandrom <amesias@thoughtworks.com>
2018-02-27 16:50:10 -0500

    Fizz buzz working

--- src/com/amesiastwu/FizzBuzzClass.java
+++ src/com/amesiastwu/FizzBuzzClass.java
@@ -13,6 +13,19 @@ public class FizzBuzzClass {
      }
 
      private String evaluateNumber(int integer){
+
+         if(integer % 3 == 0 && integer % 5 == 0){
+             return "FizzBuzz";
+         }
+
+         if(integer % 3 == 0){
+             return "Fizz";
+         }
+
+         if(integer % 5 == 0){
+             return "Buzz";
+         }
+
          return Integer.toString(integer);
      }
 }
