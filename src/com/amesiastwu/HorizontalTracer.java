6001706594369c54b1a80aee37bbc129979448e5
alejandrom <amesias@thoughtworks.com>
2018-02-27 14:31:19 -0500

    HorizontalTracer with method overloading, used in his own class

--- src/com/amesiastwu/HorizontalTracer.java
+++ src/com/amesiastwu/HorizontalTracer.java
@@ -2,12 +2,7 @@ package com.amesiastwu;
 
 public class HorizontalTracer {
     public void draw(int length){
-        final String CHARACTER = "*";
-
-        for(int i = 0 ; i < length; i++){
-            System.out.print(CHARACTER);
-        }
-
+        draw(length, "*");
     }
 
     public void draw(int length, String character){
