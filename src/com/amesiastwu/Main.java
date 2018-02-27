ad246fd46656b521d13b88dfc98078e5fbddef1a
alejandrom <amesias@thoughtworks.com>
2018-02-27 11:28:10 -0500

    Print one asterisk, ok

--- src/com/amesiastwu/Main.java
+++ src/com/amesiastwu/Main.java
@@ -3,6 +3,7 @@ package com.amesiastwu;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello Assignment");
+        System.out.println("Print one Asterisk:");
+	    System.out.print("*");
     }
 }
